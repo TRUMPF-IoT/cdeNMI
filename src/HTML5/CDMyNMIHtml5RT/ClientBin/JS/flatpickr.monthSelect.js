8178794d4739e18a69889ac18d7393ebc807ecb6
Chris Muench <chris.muench@c-labs.com>
2022-05-19 17:51:02 -0700

    Final SonarCloud fixe (#39)
    * new code sign
    
    * Latest Typescript; Copyrights to 2022
    
    * remove old entries in gitignore
    
    * removed unused license
    
    * sln update
    
    * version update
    
    * Checkbox improvement
    
    * Updates to TileButton styles with Font Awesome; latest FontAwesome; Support for autologin with a known UID
    
    * Sonar fixes
    
    * Bug Fixes and TypeScript to latest version
    
    * More fixe for SonarCloud
    
    * switch to net6
    
    * fix for gitversion
    
    * gitversion
    
    * SonarCloud fixes
    
    * packager to net48
    
    * Final SonarCloud fixes
    
    * worker result removed
    
    * proj update
    
    Co-authored-by: Chris Muench <chris.muench@live.com>
    Co-authored-by: Chris Muench <muenchris@c-labs.com>

--- src/HTML5/CDMyNMIHtml5RT/ClientBin/JS/flatpickr.monthSelect.js
+++ src/HTML5/CDMyNMIHtml5RT/ClientBin/JS/flatpickr.monthSelect.js
@@ -1,23 +1,23 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
-            (global = global || self, global.monthSelectPlugin = factory()); //NOSONAR original code
-}(this, function () {
+            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.monthSelectPlugin = factory());
+}(this, (function () {
     'use strict';
 
     /*! *****************************************************************************
-    Copyright (c) Microsoft Corporation. All rights reserved.
-    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-    this file except in compliance with the License. You may obtain a copy of the
-    License at http://www.apache.org/licenses/LICENSE-2.0
+    Copyright (c) Microsoft Corporation.
 
-    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-    MERCHANTABLITY OR NON-INFRINGEMENT.
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
 
-    See the Apache Version 2.0 License for specific language governing permissions
-    and limitations under the License.
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
     ***************************************************************************** */
 
     var __assign = function () {
@@ -33,23 +33,39 @@
 
     var monthToStr = function (monthNumber, shorthand, locale) { return locale.months[shorthand ? "shorthand" : "longhand"][monthNumber]; };
 
+    function clearNode(node) {
+        while (node.firstChild)
+            node.removeChild(node.firstChild);
+    }
+    function getEventTarget(event) {
+        try {
+            if (typeof event.composedPath === "function") {
+                var path = event.composedPath();
+                return path[0];
+            }
+            return event.target;
+        }
+        catch (error) {
+            return event.target;
+        }
+    }
+
     var defaultConfig = {
         shorthand: false,
         dateFormat: "F Y",
         altFormat: "F Y",
-        theme: "light"
+        theme: "light",
     };
     function monthSelectPlugin(pluginConfig) {
-        var config = __assign({}, defaultConfig, pluginConfig);
+        var config = __assign(__assign({}, defaultConfig), pluginConfig);
         return function (fp) {
             fp.config.dateFormat = config.dateFormat;
             fp.config.altFormat = config.altFormat;
             var self = { monthsContainer: null };
             function clearUnnecessaryDOMElements() {
-                if (!fp.rContainer || !fp.daysContainer || !fp.weekdayContainer)
+                if (!fp.rContainer)
                     return;
-                fp.rContainer.removeChild(fp.daysContainer);
-                fp.rContainer.removeChild(fp.weekdayContainer);
+                clearNode(fp.rContainer);
                 for (var index = 0; index < fp.monthElements.length; index++) {
                     var element = fp.monthElements[index];
                     if (!element.parentNode)
@@ -57,44 +73,72 @@
                     element.parentNode.removeChild(element);
                 }
             }
-            function addListeners() {
-                fp._bind(fp.prevMonthNav, "click", function () {
-                    fp.currentYear -= 1;
-                    selectYear();
-                });
-                fp._bind(fp.nextMonthNav, "mousedown", function () {
-                    fp.currentYear += 1;
-                    selectYear();
-                });
-            }
-            function addMonths() {
+            function build() {
                 if (!fp.rContainer)
                     return;
                 self.monthsContainer = fp._createElement("div", "flatpickr-monthSelect-months");
                 self.monthsContainer.tabIndex = -1;
-                fp.calendarContainer.classList.add("flatpickr-monthSelect-theme-" + config.theme);
-                for (var i = 0; i < 12; i++) {
-                    var month = fp._createElement("span", "flatpickr-monthSelect-month");
-                    month.dateObj = new Date(fp.currentYear, i);
-                    month.$i = i;
-                    month.textContent = monthToStr(i, config.shorthand, fp.l10n);
-                    month.tabIndex = -1;
-                    month.addEventListener("click", selectMonth);
-                    self.monthsContainer.appendChild(month);
-                    if ((fp.config.minDate && month.dateObj < fp.config.minDate) || (fp.config.maxDate && month.dateObj > fp.config.maxDate)) {
-                        month.classList.add("disabled");
-                    }
-                }
+                buildMonths();
                 fp.rContainer.appendChild(self.monthsContainer);
+                fp.calendarContainer.classList.add("flatpickr-monthSelect-theme-" + config.theme);
+            }
+            function buildMonths() {
+                if (!self.monthsContainer)
+                    return;
+                clearNode(self.monthsContainer);
+                var frag = document.createDocumentFragment();
+                for (var i = 0; i < 12; i++) {
+                    var month = fp.createDay("flatpickr-monthSelect-month", new Date(fp.currentYear, i), 0, i);
+                    if (month.dateObj.getMonth() === new Date().getMonth() &&
+                        month.dateObj.getFullYear() === new Date().getFullYear())
+                        month.classList.add("today");
+                    month.textContent = monthToStr(i, config.shorthand, fp.l10n);
+                    month.addEventListener("click", selectMonth);
+                    frag.appendChild(month);
+                }
+                self.monthsContainer.appendChild(frag);
+                if (fp.config.minDate &&
+                    fp.currentYear === fp.config.minDate.getFullYear())
+                    fp.prevMonthNav.classList.add("flatpickr-disabled");
+                else
+                    fp.prevMonthNav.classList.remove("flatpickr-disabled");
+                if (fp.config.maxDate &&
+                    fp.currentYear === fp.config.maxDate.getFullYear())
+                    fp.nextMonthNav.classList.add("flatpickr-disabled");
+                else
+                    fp.nextMonthNav.classList.remove("flatpickr-disabled");
+            }
+            function bindEvents() {
+                fp._bind(fp.prevMonthNav, "click", function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    fp.changeYear(fp.currentYear - 1);
+                    selectYear();
+                    buildMonths();
+                });
+                fp._bind(fp.nextMonthNav, "click", function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    fp.changeYear(fp.currentYear + 1);
+                    selectYear();
+                    buildMonths();
+                });
+                fp._bind(self.monthsContainer, "mouseover", function (e) {
+                    if (fp.config.mode === "range")
+                        fp.onMouseOver(getEventTarget(e), "flatpickr-monthSelect-month");
+                });
             }
             function setCurrentlySelected() {
                 if (!fp.rContainer)
+                    return;
+                if (!fp.selectedDates.length)
                     return;
                 var currentlySelected = fp.rContainer.querySelectorAll(".flatpickr-monthSelect-month.selected");
                 for (var index = 0; index < currentlySelected.length; index++) {
                     currentlySelected[index].classList.remove("selected");
                 }
-                var month = fp.rContainer.querySelector(".flatpickr-monthSelect-month:nth-child(" + (fp.currentMonth + 1) + ")");
+                var targetMonth = fp.selectedDates[0].getMonth();
+                var month = fp.rContainer.querySelector(".flatpickr-monthSelect-month:nth-child(" + (targetMonth + 1) + ")");
                 if (month) {
                     month.classList.add("selected");
                 }
@@ -111,42 +155,69 @@
                         selectedDate = fp.config.maxDate;
                     }
                     fp.currentYear = selectedDate.getFullYear();
-                    fp.currentYearElement.value = String(fp.currentYear);
-                    setCurrentlySelected();
                 }
+                fp.currentYearElement.value = String(fp.currentYear);
                 if (fp.rContainer) {
                     var months = fp.rContainer.querySelectorAll(".flatpickr-monthSelect-month");
                     months.forEach(function (month) {
                         month.dateObj.setFullYear(fp.currentYear);
-                        if ((fp.config.minDate && month.dateObj < fp.config.minDate) || (fp.config.maxDate && month.dateObj > fp.config.maxDate)) {
-                            month.classList.add("disabled");
+                        if ((fp.config.minDate && month.dateObj < fp.config.minDate) ||
+                            (fp.config.maxDate && month.dateObj > fp.config.maxDate)) {
+                            month.classList.add("flatpickr-disabled");
                         }
                         else {
-                            month.classList.remove("disabled");
+                            month.classList.remove("flatpickr-disabled");
                         }
                     });
                 }
+                setCurrentlySelected();
             }
             function selectMonth(e) {
                 e.preventDefault();
                 e.stopPropagation();
-                if (e.target instanceof Element && !e.target.classList.contains("disabled")) {
-                    setMonth(e.target.dateObj);
-                    fp.close();
+                var eventTarget = getEventTarget(e);
+                if (!(eventTarget instanceof Element))
+                    return;
+                if (eventTarget.classList.contains("flatpickr-disabled"))
+                    return;
+                if (eventTarget.classList.contains("notAllowed"))
+                    return; // necessary??
+                setMonth(eventTarget.dateObj);
+                if (fp.config.closeOnSelect) {
+                    var single = fp.config.mode === "single";
+                    var range = fp.config.mode === "range" && fp.selectedDates.length === 2;
+                    if (single || range)
+                        fp.close();
                 }
             }
             function setMonth(date) {
-                var selectedDate = new Date(date);
-                selectedDate.setFullYear(fp.currentYear);
-                fp.currentMonth = selectedDate.getMonth();
-                fp.setDate(selectedDate, true);
+                var selectedDate = new Date(fp.currentYear, date.getMonth(), date.getDate());
+                var selectedDates = [];
+                switch (fp.config.mode) {
+                    case "single":
+                        selectedDates = [selectedDate];
+                        break;
+                    case "multiple":
+                        selectedDates.push(selectedDate);
+                        break;
+                    case "range":
+                        if (fp.selectedDates.length === 2) {
+                            selectedDates = [selectedDate];
+                        }
+                        else {
+                            selectedDates = fp.selectedDates.concat([selectedDate]);
+                            selectedDates.sort(function (a, b) { return a.getTime() - b.getTime(); });
+                        }
+                        break;
+                }
+                fp.setDate(selectedDates, true);
                 setCurrentlySelected();
             }
             var shifts = {
                 37: -1,
                 39: 1,
                 40: 3,
-                38: -3
+                38: -3,
             };
             function onKeyDown(_, __, ___, e) {
                 var shouldMove = shifts[e.keyCode] !== undefined;
@@ -170,6 +241,26 @@
                     setMonth(document.activeElement.dateObj);
                 }
             }
+            function closeHook() {
+                var _a;
+                if (((_a = fp.config) === null || _a === void 0 ? void 0 : _a.mode) === "range" && fp.selectedDates.length === 1)
+                    fp.clear(false);
+                if (!fp.selectedDates.length)
+                    buildMonths();
+            }
+            // Help the prev/next year nav honor config.minDate (see 3fa5a69)
+            function stubCurrentMonth() {
+                config._stubbedCurrentMonth = fp._initialDate.getMonth();
+                fp._initialDate.setMonth(config._stubbedCurrentMonth);
+                fp.currentMonth = config._stubbedCurrentMonth;
+            }
+            function unstubCurrentMonth() {
+                if (!config._stubbedCurrentMonth)
+                    return;
+                fp._initialDate.setMonth(config._stubbedCurrentMonth);
+                fp.currentMonth = config._stubbedCurrentMonth;
+                delete config._stubbedCurrentMonth;
+            }
             function destroyPluginInstance() {
                 if (self.monthsContainer !== null) {
                     var months = self.monthsContainer.querySelectorAll(".flatpickr-monthSelect-month");
@@ -180,25 +271,32 @@
             }
             return {
                 onParseConfig: function () {
-                    fp.config.mode = "single";
                     fp.config.enableTime = false;
                 },
                 onValueUpdate: setCurrentlySelected,
                 onKeyDown: onKeyDown,
                 onReady: [
+                    stubCurrentMonth,
                     clearUnnecessaryDOMElements,
-                    addListeners,
-                    addMonths,
+                    build,
+                    bindEvents,
                     setCurrentlySelected,
                     function () {
+                        fp.config.onClose.push(closeHook);
                         fp.loadedPlugins.push("monthSelect");
                     },
                 ],
-                onDestroy: destroyPluginInstance
+                onDestroy: [
+                    unstubCurrentMonth,
+                    destroyPluginInstance,
+                    function () {
+                        fp.config.onClose = fp.config.onClose.filter(function (hook) { return hook !== closeHook; });
+                    },
+                ],
             };
         };
     }
 
     return monthSelectPlugin;
 
-}));
+})));
